314dc7cd5c7711044587a8d11584e1f9d77ef8ee
Nicolas Franz-Pattillo <9399502+nicofranzp@users.noreply.github.com>
2025-01-03 18:35:55 -0300

    fix compile errors

--- keyboards/lily58/keymaps/nicofranzp/keymap.c
+++ keyboards/lily58/keymaps/nicofranzp/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LCTL,  HOME_A,  HOME_S,  HOME_D,  HOME_F,  KC_G,                     KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, MO(_MOUSE),  DT_PRNT,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
                         XXXXXXX, MO(_NUMPAD), MO(_SYMBOLS), MT(MOD_LGUI,KC_ENT), MT(MOD_LGUI,KC_SPC), KC_BSPC, MO(_ARROWS), XXXXXXX
-)
+),
 
 /* SYMBOLS
  * ,-----------------------------------------.                    ,-----------------------------------------.
