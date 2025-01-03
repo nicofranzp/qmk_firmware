c972e3ca2b825fbdfae82ab8bd2448dfd9616c04
Nicolas Franz-Pattillo <9399502+nicofranzp@users.noreply.github.com>
2025-01-03 19:19:06 -0300

    homerow mods working

--- keyboards/lily58/keymaps/nicofranzp/rules.mk
+++ keyboards/lily58/keymaps/nicofranzp/rules.mk
@@ -12,3 +12,7 @@ VIA_ENABLE = yes
 MOUSEKEY_ENABLE = yes
 EXTRAKEY_ENABLE = yes
 DYNAMIC_TAPPING_TERM_ENABLE= yes
+SRC +=  ./lib/rgb_state_reader.c \
+        ./lib/layer_state_reader.c \
+        ./lib/logo_reader.c \
+        ./lib/keylogger.c \
